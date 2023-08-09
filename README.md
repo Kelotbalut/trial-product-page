ee8c7e2d321e930282454e546beda292bf3c3043
Kelotbalut <130615045+Kelotbalut@users.noreply.github.com>
2023-08-09 22:49:19 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# trial-product-page
+# Steps to run project:
+
+1. npm i - this will download all dependencies.
+2. npm run build-css - builds the initial CSS files
+3. npm run dev - Starts the express server. In the terminal you will see where you can access it.
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const app = express()
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs')
+
+app.use(require('express-ejs-layouts'))
+
+app.set('layout', './layouts/master')
+
+// Products page
+app.get('/products', (req, res) => {
+  const viewsData = {
+    items: items,
+    sizes: sizes,
+  }
+  res.render('pages/products', viewsData)
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
+
+const browserSync = require('browser-sync').create()
+const bsConfig = require('./bs-config.js')
+const { items } = require('./constants/items.js')
+const { sizes } = require('./constants/sizes.js')
+browserSync.init(bsConfig)
+app.use(require('connect-browser-sync')(browserSync))
+
+app.use(express.static('public'))
